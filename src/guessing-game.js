ca0536dcbdd84b12fd0a2d556a0317c39dd69966
ragexe <ragexe@mail.ru>
2017-09-13 00:41:36 +0300

    guessing-game/feature: necessary methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.solutionCandidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.solutionCandidate = Math.ceil((this.max-this.min)/2+this.min);
     }
 
     lower() {
-
+        this.setRange(this.min, this.solutionCandidate);
     }
 
     greater() {
-
+        this.setRange(this.solutionCandidate, this.max);
     }
 }
 
